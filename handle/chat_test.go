ecd032dfd58cd7568c5d247d00cf13a4ad13584a
root <rinai@g-rinai.cn>
2025-05-10 22:04:15 +0800

    ci

--- handle/chat_test.go
+++ handle/chat_test.go
@@ -12,14 +12,12 @@ func TestChat(t *testing.T) {
 		HandshakeTimeout: 5 * time.Second,
 	}
 
-	// 注意：URL 一定要带 ws://
 	conn, _, err := dialer.Dial("ws://localhost:9999/chat", nil)
 	if err != nil {
 		t.Fatal("连接失败:", err)
 	}
 	defer conn.Close()
 
-	// 向服务器发送一个简单的消息
 	err = conn.WriteMessage(websocket.TextMessage, []byte("ping"))
 	if err != nil {
 		t.Fatal("向服务器发送消息失败", err)
